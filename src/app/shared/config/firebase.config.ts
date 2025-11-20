f73f977111624238c26188d25c478460f5baf3fc
PPrez24 <eduardo.perez@iteso.mx>
2025-11-19 22:30:51 -0600

    credenciales borradas

--- src/app/shared/config/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAU0JoHTKZWSU6l1Zy-WpkPUluEz52FFqM",
-  authDomain: "proyecto-cliente-9696a.firebaseapp.com",
-  projectId: "proyecto-cliente-9696a",
-  storageBucket: "proyecto-cliente-9696a.firebasestorage.app",
-  messagingSenderId: "340851957870",
-  appId: "1:340851957870:web:6a276dcbdc5214100a867f"
-};
