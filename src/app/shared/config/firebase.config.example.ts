954113a15a5d610cb8d2b557e2af9ffcca387e60
PPrez24 <eduardo.perez@iteso.mx>
2025-12-06 19:28:25 -0600

    deploy

--- src/app/shared/config/firebase.config.example.ts
+++
@@ -1,9 +0,0 @@
-// Firebase client config - public keys for web SDK
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
